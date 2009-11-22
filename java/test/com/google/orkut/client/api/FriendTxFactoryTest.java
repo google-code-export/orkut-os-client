3fd36e050db83f3a3e2f62f80b210604591225a4
sachins <sachins@opgntd-ubiq29.hyd.corp.google.com>
2009-11-22 03:54:16 -0800

    added tests for all friend invite tx

--- java/test/com/google/orkut/client/api/FriendTxFactoryTest.java
+++ java/test/com/google/orkut/client/api/FriendTxFactoryTest.java
@@ -82,6 +82,78 @@ public class FriendTxFactoryTest extends TestCase {
     assertEquals("newui10", e.getUserProfile().getGivenName());
     assertEquals("http://img2.orkut.com/images/small/5678/5678/photo.jpg",
         e.getUserProfile().getThumbnailUrl());
-    
+  }
+  
+  public void testSendFriendRequest() throws Exception {
+    FriendRequestTx requestTx = factory.sendFriendRequest("3456");
+    JSONObject json = requestTx.getRequestAsJson();
+    json.remove("id");
+    JSONObject req = new JSONObject(
+        "{'params':{'groupId':'@friends','userId':'@me','personId':'3456'}," +
+        "'method':'people.sendFriendRequest'}");
+    assertTrue(comparator.isEquals(json, req));
+  }
+  
+  public void testSendFriendRequestWithImAndNote() throws Exception {
+    FriendRequestTx requestTx = factory.sendFriendRequest("3456");
+    requestTx.setAllowIm(true).setNote("Make me friend");
+    JSONObject json = requestTx.getRequestAsJson();
+    json.remove("id");
+    JSONObject req = new JSONObject(
+        "{'params':{'groupId':'@friends','userId':'@me','personId':'3456'," +
+        "'allowIm':true, 'note':'Make me friend'}," +
+        "'method':'people.sendFriendRequest'}");
+    assertTrue(comparator.isEquals(json, req));
+  }
+  
+  public void testAcceptFriendRequest() throws Exception {
+    FriendRequestTx requestTx = factory.acceptFriendRequest("3456");
+    JSONObject json = requestTx.getRequestAsJson();
+    json.remove("id");
+    JSONObject req = new JSONObject(
+        "{'params':{'groupId':'@friends','userId':'@me','personId':'3456'}," +
+        "'method':'people.acceptFriendRequest'}");
+    assertTrue(comparator.isEquals(json, req));
+  }
+
+  public void testAcceptFriendRequestWithIm() throws Exception {
+    FriendRequestTx requestTx = factory.acceptFriendRequest("3456");
+    requestTx.setAllowIm(true);
+    JSONObject json = requestTx.getRequestAsJson();
+    json.remove("id");
+    JSONObject req = new JSONObject(
+        "{'params':{'groupId':'@friends','userId':'@me','personId':'3456', 'allowIm':true}," +
+        "'method':'people.acceptFriendRequest'}");
+    assertTrue(comparator.isEquals(json, req));
+  }
+
+  public void testRevokeFriendRequest() throws Exception {
+    Transaction requestTx = factory.revokeFriendRequest("3456");
+    JSONObject json = requestTx.getRequestAsJson();
+    json.remove("id");
+    JSONObject req = new JSONObject(
+        "{'params':{'groupId':'@friends','userId':'@me','personId':'3456'}," +
+        "'method':'people.revokeFriendRequest'}");
+    assertTrue(comparator.isEquals(json, req));
+  }
+
+  public void testRemoveFriend() throws Exception {
+    Transaction requestTx = factory.removeFriendRequest("3456");
+    JSONObject json = requestTx.getRequestAsJson();
+    json.remove("id");
+    JSONObject req = new JSONObject(
+        "{'params':{'groupId':'@friends','userId':'@me','personId':'3456'}," +
+        "'method':'people.removeFriend'}");
+    assertTrue(comparator.isEquals(json, req));
+  }
+  
+  public void testRejectFriendRequest() throws Exception {
+    Transaction requestTx = factory.rejectFriendRequest("3456");
+    JSONObject json = requestTx.getRequestAsJson();
+    json.remove("id");
+    JSONObject req = new JSONObject(
+        "{'params':{'groupId':'@friends','userId':'@me','personId':'3456'}," +
+        "'method':'people.rejectFriendRequest'}");
+    assertTrue(comparator.isEquals(json, req));
   }
 }
